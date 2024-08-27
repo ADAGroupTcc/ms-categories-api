9ea732e2a11b892efb27a010f94cd63b77164849
rafael-ferraz-s <rafael.ferraz@picpay.com>
2024-08-27 07:24:55 -0300

    feat: category's domain with conversion and validation functions

--- exceptions/exceptions.go
+++ exceptions/exceptions.go
@@ -4,19 +4,17 @@ import (
 	"fmt"
 )
 
-const prefix = "users-api"
+const prefix = "categories-api"
 
 var (
 
 	// Errors related to request validation
-	ErrInvalidPayload    = fmt.Errorf("%s: invalid payload", prefix)
-	ErrInvalidFirstName  = fmt.Errorf("%s: invalid first name", prefix)
-	ErrInvalidLastName   = fmt.Errorf("%s: invalid last name", prefix)
-	ErrInvalidEmail      = fmt.Errorf("%s: invalid email", prefix)
-	ErrInvalidCPF        = fmt.Errorf("%s: invalid CPF", prefix)
-	ErrInvalidCategories = fmt.Errorf("%s: invalid categories", prefix)
-	ErrUserAlreadyExists = fmt.Errorf("%s: user already exists", prefix)
-	ErrInvalidID         = fmt.Errorf("%s: invalid ID", prefix)
+	ErrInvalidPayload        = fmt.Errorf("%s: invalid payload", prefix)
+	ErrInvalidName           = fmt.Errorf("%s: invalid category's name", prefix)
+	ErrInvalidDescription    = fmt.Errorf("%s: invalid category's descriptionn", prefix)
+	ErrInvalidClassification = fmt.Errorf("%s: invalid category's classification", prefix)
+	ErrCategoryAlreadyExists = fmt.Errorf("%s: category already exists", prefix)
+	ErrInvalidID             = fmt.Errorf("%s: invalid ID", prefix)
 
 	// Database related errors
 	ErrUserNotFound    = fmt.Errorf("%s: user not found", prefix)
--- internal/domain/domain.go
+++ internal/domain/domain.go
@@ -1,1 +1,87 @@
 package domain
+
+import (
+	"github.com/ADAGroupTcc/ms-categories-api/exceptions"
+	"github.com/ADAGroupTcc/ms-categories-api/pkg/mongorm"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+type Category struct {
+	mongorm.Model  `json:",inline" bson:",inline"`
+	Name           string `json:"name" bson:"name"`
+	Description    string `json:"description" bson:"description"`
+	Classification int    `json:"classification" bson:"classification"`
+}
+
+type CategoriesResponse struct {
+	Categories []Category `json:"categories"`
+	NextPage   int64      `json:"next_page,omitempty"`
+}
+
+type CategoryRequest struct {
+	Name           string `json:"name"`
+	Description    string `json:"description"`
+	Classification int    `json:"classification"`
+}
+
+func (c *CategoryRequest) ToCategory() Category {
+	return Category{
+		Name:           c.Name,
+		Description:    c.Description,
+		Classification: c.Classification,
+	}
+}
+
+func (c *CategoryRequest) Validate() error {
+	if c.Name == "" || len(c.Name) < 3 {
+		return exceptions.New(exceptions.ErrInvalidName, nil)
+	}
+	if c.Description == "" || len(c.Description) < 3 {
+		return exceptions.New(exceptions.ErrInvalidDescription, nil)
+	}
+
+	if c.Classification <= 0 || c.Classification > 5 {
+		return exceptions.New(exceptions.ErrInvalidClassification, nil)
+	}
+
+	return nil
+}
+
+type CategoryPatchRequest struct {
+	Name           *string `json:"name"`
+	Description    *string `json:"description"`
+	Classification *int    `json:"classification"`
+}
+
+func (c *CategoryPatchRequest) Validate() error {
+	if c.Name != nil && *c.Name == "" || len(*c.Name) < 3 {
+		return exceptions.New(exceptions.ErrInvalidName, nil)
+	}
+	if c.Description != nil && *c.Description == "" || len(*c.Description) < 3 {
+		return exceptions.New(exceptions.ErrInvalidDescription, nil)
+	}
+
+	if c.Classification != nil && *c.Classification <= 0 || *c.Classification > 5 {
+		return exceptions.New(exceptions.ErrInvalidClassification, nil)
+	}
+
+	return nil
+}
+
+func (c *CategoryPatchRequest) ToBsonM() bson.M {
+	updateFields := bson.M{}
+
+	if c.Name != nil {
+		updateFields["name"] = *c.Name
+	}
+
+	if c.Description != nil {
+		updateFields["description"] = *c.Description
+	}
+
+	if c.Classification != nil {
+		updateFields["classification"] = *c.Classification
+	}
+
+	return bson.M{"$set": updateFields}
+}
